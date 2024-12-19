32be61fa9cf58366fca49072e5fdc75669fb1ce6
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-12-19 18:21:30 +0200

    featcreate and  add preloader

--- dear-diary/src/components/Main/Main.jsx
+++ dear-diary/src/components/Main/Main.jsx
@@ -1,11 +1,18 @@
+import React, { useState, useEffect } from "react";
+import Preloader from "../Preloader/Preloader";
 import "./Main.css";
 function Main({ advice, onRefresh }) {
   return (
     <main className="main">
       <div className={`main__advice ${!advice ? "main__advice--loading" : ""}`}>
-        <p className="main__advice-text">
-          {advice ? `Guru Advices: ${advice}` : "Loading advice..."}
-        </p>
+        {advice ? (
+          <p className="main__advice-text">Guru Advices: {advice}</p>
+        ) : (
+          <>
+            <Preloader />
+            <p className="main__advice-text">Loading advice...</p>
+          </>
+        )}
         <button
           className="main__advice-change-btn"
           type="button"
