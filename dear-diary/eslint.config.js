7f446f659bb505924b0b6089baa53ebef0fb5a58
Aleksandr Goldshtern <alexg020573@gmail.com>
2024-12-26 20:53:27 +0200

    refactor: eslint.config.js

--- dear-diary/eslint.config.js
+++ dear-diary/eslint.config.js
@@ -5,19 +5,22 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 
 export default [
-  { ignores: ["dist"] },
+  {
+    ignores: ["dist"],
+  },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
-      ecmaVersion: 2020,
+      ecmaVersion: "latest",
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
         ecmaFeatures: { jsx: true },
         sourceType: "module",
       },
     },
-    settings: { react: { version: "18.3" } },
+    settings: {
+      react: { version: "18.3" },
+    },
     plugins: {
       react,
       "react-hooks": reactHooks,
@@ -28,18 +31,16 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
+
       "react/jsx-no-target-blank": "off",
       "react-refresh/only-export-components": [
         "warn",
         { allowConstantExport: true },
       ],
-      "react/prop-types": 0,
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-uses-react": "off",
+      "no-unused-vars": ["error", { varsIgnorePattern: "^React$" }],
     },
-    overrides: [
-      {
-        // Without this, `npx eslint .` doesn't run on jsx files.
-        files: ["*.js", "*.jsx"],
-      },
-    ],
   },
 ];
