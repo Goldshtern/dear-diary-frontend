7be4dfc5db6516f43f8111a05c713f3fcc3b28c8
Aleksandr Goldshtern <alexg020573@gmail.com>
2025-02-21 13:40:06 +0200

    feat: add delete button markup at DiaryPage.jsx and DiaryPage.css

--- src/components/DiaryPage/DiaryPage.jsx
+++ src/components/DiaryPage/DiaryPage.jsx
@@ -1,11 +1,15 @@
 import "./DiaryPage.css";
+import deleteBtn from "../../assets/deleterip-button.png";
 
 function DiaryPage({ item }) {
   return (
     <li className="diary-page">
       <h2 className="diary-page__name">{item.title}</h2>
       <img className="diary-page__image" src={item.imageUrl} alt={item.name} />
-      <p className="diary-page__text">{item.text}</p>
+      <p className="diary-page_text">{item.text}</p>
+      <button type="button" className="diary-page__delete">
+        <img src={deleteBtn} alt="Delete" />
+      </button>
     </li>
   );
 }
